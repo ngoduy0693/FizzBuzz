1ab67b56d8bc51ffce5ec2d1d7e1f6e896cf44d7
ngoduy0693 <duynv@sphinx-software.com>
2019-03-12 09:22:03 +0700

    fizzbuzz the first

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+for(var i = 1 ;i <= 100; i++)
+{
+	if(i%3 ==0 && i%5 == 0)
+	{
+		console.log("sphinx-software");
+	} else if(i%3 == 0)
+	{
+		console.log("sphinx");
+	}else if(i%5 == 0)
+	{
+		console.log("software");
+	}else 
+	{
+		console.log(i);
+	}
+}
